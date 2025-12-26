3492570846b27cfd5f740369278f08a67abd0771
Sonu Yadav <sy239370@gmail.com>
2025-12-26 15:22:11 +0530

    inputing Primitives

---
+++ Basic Java Recap/src/com/basic/InputPrimitive.java
@@ -0,0 +1,11 @@
+package com.basic;
+
+import java.util.Scanner;
+
+public class InputPrimitive {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int rollno = input.nextInt();
+        System.out.println("Your roll no is: " + rollno);
+    }
+}
--- Basic Java Recap/src/com/basic/Main.java
+++ Basic Java Recap/src/com/basic/Main.java
@@ -3,6 +3,9 @@ package com.basic;
 import java.util.Scanner;
 
 public class Main {
+//    if file name is Main.java then
+//    it's public class should be same name as it's file name e.g Main
+//    if not public he can put anything as his class Name
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
         System.out.println("<----New world---->");
