82b6cab66038c4c396abea932aa15d2575371c2e
Sonu Yadav <sy239370@gmail.com>
2025-12-28 23:07:31 +0530

    Highest common factor and lowest common factor problem added in basics

---
+++ Basic Java Recap/src/com/basic/Commonfact.java
@@ -0,0 +1,22 @@
+package com.basic;
+
+import java.util.Scanner;
+
+public class Commonfact {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter your two numbers: ");
+        int num1 = input.nextInt();
+        int num2 = input.nextInt();
+        int hcf = 1;
+        for (int i = 1; i <= num1 && i <= num2; i++){
+            if(num1%i==0 && num2%i==0){
+                hcf = i;
+            }
+        }
+
+        int lcm = (num1*num2)/hcf;
+
+        System.out.println("Lcm is:" + lcm + "\nHCF is:"+ hcf);
+    }
+}
--- Basic Java Recap/src/com/basic/Temperature.java
+++ Basic Java Recap/src/com/basic/Temperature.java
@@ -8,7 +8,7 @@ public class Temperature {
         System.out.println("<--Celsius to Fahrenheit Temperature convertor-->");
         System.out.print("Enter Your Temperature in Celsius: ");
         float c = input.nextFloat();
-        float f = c*9/5 + 32;
+        float f = 9/5 + 32;
         System.out.println("Temperature in fahrenheit: " +f);
     }
 }
