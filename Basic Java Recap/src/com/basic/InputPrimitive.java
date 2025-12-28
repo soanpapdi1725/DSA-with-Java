ce7b033a1393a19c744f55ecc75a16bf9d888aab
Sonu Yadav <sy239370@gmail.com>
2025-12-28 20:14:35 +0530

    typecasting learnt

--- Basic Java Recap/src/com/basic/InputPrimitive.java
+++ Basic Java Recap/src/com/basic/InputPrimitive.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class InputPrimitive {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        System.out.print("Please Enter your rollno: ");
         int rollno = input.nextInt();
         System.out.println("Your roll no is: " + rollno);
     }
