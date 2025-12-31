8ff043d4cb2963e9eaa580aed50304f90e4bbc33
Sonu Yadav <sy239370@gmail.com>
2025-12-31 20:55:53 +0530

    Conditionals and Loops

---
+++ Basic Java Recap/src/com/conditionalsAndLoops/Loops.java
@@ -0,0 +1,49 @@
+package com.conditionalsAndLoops;
+
+import java.util.Scanner;
+
+public class Loops {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+//        Loops are used for performing something which needs to be done continously
+        /*
+        for loop
+         * for(initilialization; condition; increment/decrement){
+         *     //block of code
+         * }
+         * */
+
+//        System.out.print("Enter your number");
+//        int num = input.nextInt();
+//        for (int i = 0; i <= num; i = i + 2) {
+//            System.out.print(i + " ");
+//        }
+
+        /*
+         * while loop
+         * initialization
+         * while(condition){
+         * increment/decrement
+         * }
+         * */
+//        System.out.print("Enter your number: ");
+//        int num = input.nextInt();
+//        int i = 0;
+//        while (i <= num) {
+//            System.out.print(i + " ");
+//            i = i + 2;
+//        }
+
+
+        /*
+         * while loop
+         * initialization
+         * while(condition){
+         * increment/decrement
+         * }
+         * */
+        System.out.print("Enter your number: ");
+        int num = input.nextInt();
+    }
+}
