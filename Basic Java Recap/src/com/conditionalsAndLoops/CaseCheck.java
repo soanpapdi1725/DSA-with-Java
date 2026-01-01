ec2af5188b71d86b2a911a528deb09002f7bb9f3
Sonu Yadav <sy239370@gmail.com>
2026-01-01 20:56:46 +0530

    conditionals and loops video completed

--- Basic Java Recap/src/com/conditionalsAndLoops/CaseCheck.java
+++ Basic Java Recap/src/com/conditionalsAndLoops/CaseCheck.java
@@ -7,7 +7,7 @@ public class CaseCheck {
         Scanner input = new Scanner(System.in);
         System.out.print("Enter the word: ");
         char firstLetter = input.next().trim().charAt(0);
-        if (firstLetter < 97) {
+        if (!(firstLetter > 'a' && firstLetter < 'z')) {
             System.out.println("first letter was Capital");
         } else {
             System.out.println("first letter was small");
---
+++ Basic Java Recap/src/com/conditionalsAndLoops/ReverseNum.java
@@ -0,0 +1,18 @@
+package com.conditionalsAndLoops;
+
+import java.util.Scanner;
+
+public class ReverseNum {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter number to reverse: ");
+        int num = input.nextInt();
+        int rev = 0;
+        while (num > 0) {
+            int rem = num % 10;
+            rev = rev * 10 + rem;
+            num = num / 10;
+        }
+        System.out.println("Here is your reversed number: " + rev);
+    }
+}
