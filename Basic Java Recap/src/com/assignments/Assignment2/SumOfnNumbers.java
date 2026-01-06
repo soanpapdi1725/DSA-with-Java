533b359d0d15876b1c1bd4747991ae0920265dc8
Sonu Yadav <sy239370@gmail.com>
2026-01-06 23:23:43 +0530

    Solving the problem

--- Basic Java Recap/src/com/assignments/Assignment2/SumOfnNumbers.java
+++ Basic Java Recap/src/com/assignments/Assignment2/SumOfnNumbers.java
@@ -1,7 +1,32 @@
 package com.assignments.Assignment2;
 
+import java.util.Scanner;
+
 public class SumOfnNumbers {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int sum = 0;
+        int count = 0;
+        String expression = "";
+        while (true) {
+            System.out.println("Choose A valid option from below:");
+            System.out.println("1. Add one more number");
+            System.out.println("2. Calculate sum");
+            int option = input.nextInt();
+            if (option == 1) {
+                System.out.print(expression);
+                int num = input.nextInt();
+                sum = sum + num;
 
+                expression += num + " + ";
+                count++;
+            } else if (option == 2) {
+                System.out.println(expression.substring(0, expression.length() - 3) + " = " + sum);
+                break;
+            } else {
+                System.out.println("Invalid option is choosen");
+                break;
+            }
+        }
     }
 }
--- Basic Java Recap/src/com/assignments/Assignment2/SumofDigits.java
+++ Basic Java Recap/src/com/assignments/Assignment2/SumofDigits.java
@@ -1,4 +1,7 @@
 package com.assignments.Assignment2;
 
 public class SumofDigits {
+    public static void main(String[] args) {
+
+    }
 }
