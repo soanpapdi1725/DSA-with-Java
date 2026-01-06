bcf1de7218903a17b2ddc50efbff7d9ac3519021
Sonu Yadav <sy239370@gmail.com>
2026-01-07 01:08:56 +0530

    Some armstrong number check done

--- Basic Java Recap/src/com/assignments/Assignment2/ArmstrongNumber.java
+++ Basic Java Recap/src/com/assignments/Assignment2/ArmstrongNumber.java
@@ -1,4 +1,23 @@
 package com.assignments.Assignment2;
 
+import java.util.Scanner;
+
 public class ArmstrongNumber {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the number to check the Armstrong number: ");
+        int num = input.nextInt();
+        int temp = num;
+        int sum = 0;
+        while (temp > 0) {
+            int rem = temp % 10;
+            sum += rem * rem * rem;
+            temp /= 10;
+        }
+        if (num == sum) {
+            System.out.println("It is Armstrong number");
+        } else {
+            System.out.println("It is not armstrong number");
+        }
+    }
 }
