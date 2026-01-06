98d1777e100ee7a44e41104bae9bc889399f5bc2
Sonu Yadav <sy239370@gmail.com>
2026-01-07 01:02:52 +0530

    Some palindrome check done

--- Basic Java Recap/src/com/assignments/Assignment2/PalindromeCheck.java
+++ Basic Java Recap/src/com/assignments/Assignment2/PalindromeCheck.java
@@ -5,6 +5,19 @@ import java.util.Scanner;
 public class PalindromeCheck {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-
+        System.out.print("Enter number to check the palindrome: ");
+        int num = input.nextInt();
+        int temp = num;
+        int rev = 0;
+        while (temp > 0) {
+            int rem = temp % 10;
+            rev = rev * 10 + rem;
+            temp /= 10;
+        }
+        if (num == rev) {
+            System.out.println("It is palindrome");
+        } else {
+            System.out.println("It is not palindrome");
+        }
     }
 }
