e0c8a915cfd4fa7bbbc460cc2b436871fd4cd4d6
Sonu Yadav <sy239370@gmail.com>
2026-01-15 19:11:18 +0530

    Functions and methods have been started

---
+++ Basic Java Recap/src/com/FunctionAndMethod/StringExample.java
@@ -0,0 +1,13 @@
+package com.FunctionAndMethod;
+
+import java.util.Scanner;
+
+public class StringExample {
+    public static void main() {
+        Scanner
+    }
+    static String greeting(String greets){
+        System.out.println(greets);
+        return greets;
+    }
+}
