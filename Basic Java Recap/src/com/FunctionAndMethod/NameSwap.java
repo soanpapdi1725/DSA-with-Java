ed71bd91aaca16ee72cdeabc028c4519ee3cb7d0
Sonu Yadav <sy239370@gmail.com>
2026-01-18 20:00:31 +0530

    understood difference between primitives and objects(array & object) for swap value

--- Basic Java Recap/src/com/FunctionAndMethod/NameSwap.java
+++ Basic Java Recap/src/com/FunctionAndMethod/NameSwap.java
@@ -10,4 +10,7 @@ public class NameSwap {
     static void changeName(String naam) { // copy of name is passed name = "Sonu Yadav"
         naam = "Sahil Yadav"; // now it is pointing to different object or value in the heap which is "Sahil yadav"
     }
+
+//    this is not working because we cannot directly modify the string
+//    Strings and arrays are objects in this the copy of the entire array or strings is passed in function
 }
