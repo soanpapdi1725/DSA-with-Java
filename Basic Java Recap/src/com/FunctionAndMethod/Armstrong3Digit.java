d40329ca296af1c0f51e0e9ca883be65fcbb783b
Sonu Yadav <sy239370@gmail.com>
2026-01-19 19:05:07 +0530

    isPrime done using function

---
+++ Basic Java Recap/src/com/FunctionAndMethod/Armstrong3Digit.java
@@ -0,0 +1,7 @@
+package com.FunctionAndMethod;
+
+public class Armstrong3Digit {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ Basic Java Recap/src/com/FunctionAndMethod/primeNumberCheck.java
@@ -0,0 +1,28 @@
+package com.FunctionAndMethod;
+
+import java.util.Scanner;
+
+public class primeNumberCheck {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the number to check Prime Number: ");
+//        int num = input.nextInt();
+        for (int i = 2; i <= 100; i++) {
+            if (isPrime(i)) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+
+    static boolean isPrime(int n) {
+        if (n <= 1) {
+            return false;
+        }
+        for (int i = 2; i * i <= n; i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
