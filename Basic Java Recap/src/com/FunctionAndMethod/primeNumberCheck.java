fa7717a116d9fef176b7a8c1d8ff9648622dff6b
Sonu Yadav <sy239370@gmail.com>
2026-01-19 19:30:28 +0530

    isArmstrong done using function

--- Basic Java Recap/src/com/FunctionAndMethod/primeNumberCheck.java
+++ Basic Java Recap/src/com/FunctionAndMethod/primeNumberCheck.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class primeNumberCheck {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.print("Enter the number to check Prime Number: ");
+//        System.out.print("Enter the number to check Prime Number: ");
 //        int num = input.nextInt();
         for (int i = 2; i <= 100; i++) {
             if (isPrime(i)) {
