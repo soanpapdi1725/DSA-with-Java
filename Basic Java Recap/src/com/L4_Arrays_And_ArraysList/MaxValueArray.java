0d34b08ed08f9eb1a01a4213b49b3d49d9be47d4
Sonu Yadav <sy239370@gmail.com>
2026-01-28 21:38:10 +0530

    Max value in array question with range now

--- Basic Java Recap/src/com/L4_Arrays_And_ArraysList/MaxValueArray.java
+++ Basic Java Recap/src/com/L4_Arrays_And_ArraysList/MaxValueArray.java
@@ -1,5 +1,6 @@
 package com.L4_Arrays_And_ArraysList;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class MaxValueArray {
@@ -10,10 +11,16 @@ public class MaxValueArray {
             System.out.print("Enter the " + (i + 1) + " element: ");
             arr[i] = input.nextInt();
         }
+        System.out.println(Arrays.toString(arr));
         System.out.println("Max value of your array is: " + maxVal(arr));
+        System.out.println("Max value in range of your array is: " + maxValInRange(arr, 5, 6));
     }
 
     static int maxVal(int[] array) {
+        if (array.length == 0) {
+            return -1;
+        }
+
         int maxElement = array[0];
         for (int i = 1; i < array.length; i++) {
             if (array[i] > maxElement) {
@@ -22,4 +29,21 @@ public class MaxValueArray {
         }
         return maxElement;
     }
+
+    static int maxValInRange(int[] array, int start, int end) {
+        if (array.length == 0) {
+            return -1;
+        }
+        if (start == end) {
+            return array[start];
+        }
+        int maxValue = array[start];
+        for (int i = start; i < end; i++) {
+            if (array[i] > maxValue) {
+                maxValue = array[i];
+            }
+        }
+        return maxValue;
+    }
+
 }
