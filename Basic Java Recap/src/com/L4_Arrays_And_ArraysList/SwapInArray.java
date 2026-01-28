210b9a2d79098b7b224a7397dbda8a9d4e00384d
Sonu Yadav <sy239370@gmail.com>
2026-01-28 21:10:30 +0530

    Swap using index in array

---
+++ Basic Java Recap/src/com/L4_Arrays_And_ArraysList/SwapInArray.java
@@ -0,0 +1,25 @@
+package com.L4_Arrays_And_ArraysList;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class SwapInArray {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int[] arr = {55, 5, 96, 15, 25, 63};
+        System.out.print("Enter the index1: ");
+        int index1 = input.nextInt();
+        System.out.print("Enter the index2: ");
+        int index2 = input.nextInt();
+        int[] array = swap(arr, index1, index2);
+        System.out.println(Arrays.toString(array));
+    }
+
+    static int[] swap(int[] arr, int index1, int index2) {
+        int temp = arr[index1];
+        arr[index1] = arr[index2];
+        arr[index2] = temp;
+        return arr;
+    }
+
+}
