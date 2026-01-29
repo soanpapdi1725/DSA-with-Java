ad4dcc6172fe9728aef3532d441b829e22490471
Sonu Yadav <sy239370@gmail.com>
2026-01-30 01:33:35 +0530

    2 sum in leetcode understood with good time complexity

--- Basic Java Recap/src/com/L4_Arrays_And_ArraysList/LinearSearch/EvenDigits.java
+++ Basic Java Recap/src/com/L4_Arrays_And_ArraysList/LinearSearch/EvenDigits.java
@@ -1,7 +1,7 @@
 package com.L4_Arrays_And_ArraysList.LinearSearch;
 
 // https://leetcode.com/problems/find-numbers-with-even-number-of-digits/
-class Solution {
+class EvenDigits {
     public static void main(String[] args) {
         int[] arr = {12, 345, 2, 6, 7896};
         System.out.println(findNumbers(arr));
@@ -11,16 +11,21 @@ class Solution {
     static int findNumbers(int[] nums) {
         int evenDigits = 0;
         for (int element : nums) {
-            int count = 0;
-            int num = element;
-            while (num != 0) {
-                num /= 10;
-                count++;
-            }
-            if (count % 2 == 0) {
+            if (isEvenDigit(element)) {
                 evenDigits++;
             }
         }
         return evenDigits;
     }
+
+    static boolean isEvenDigit(int num) {
+        int count = 0;
+        while (num != 0) {
+            num /= 10;
+            count++;
+        }
+        return count % 2 == 0;
+    }
+
+
 }
---
+++ Basic Java Recap/src/com/L4_Arrays_And_ArraysList/LinearSearch/TwoSumLeet.java
@@ -0,0 +1,23 @@
+package com.L4_Arrays_And_ArraysList.LinearSearch;
+
+import java.util.Arrays;
+
+public class TwoSumLeet {
+    public static void main(String[] args) {
+        int[] nums = {2, 7, 11, 15};
+        System.out.println(Arrays.toString(twoSum(nums, 27)));
+    }
+
+    static int[] twoSum(int[] nums, int target) {
+        for (int i = 1; i < nums.length; i++) {
+            for (int j = i; j < nums.length; j++) {
+                int sum = nums[j] + nums[j - i];
+                if (sum == target) {
+                    return new int[]{j, j - i};
+                }
+            }
+        }
+        return null;
+    }
+
+}
