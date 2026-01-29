83421b9b4524c8bf6f85991b4f080b217e611a76
Sonu Yadav <sy239370@gmail.com>
2026-01-29 21:19:39 +0530

    Intro of Linear Search is done

--- Basic Java Recap/src/com/L4_Arrays_And_ArraysList/ReverseArray.java
+++ Basic Java Recap/src/com/L4_Arrays_And_ArraysList/ReverseArray.java
@@ -1,4 +1,20 @@
 package com.L4_Arrays_And_ArraysList;
 
+import java.util.Arrays;
+
 public class ReverseArray {
+    public static void main(String[] args) {
+        int[] array = {55, 59, 15, 8, 97, 69};
+        reverseArray(array);
+        System.out.println(Arrays.toString(array));
+    }
+
+    static void reverseArray(int[] arr) {
+        int temp;
+        for (int i = 0; i < arr.length / 2; i++) {
+            temp = arr[i];
+            arr[i] = arr[arr.length - i - 1];
+            arr[arr.length - i - 1] = temp;
+        }
+    }
 }
