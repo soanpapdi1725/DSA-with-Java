b7224eb097322ce674dabc72490333413d5512ec
Sonu Yadav <sy239370@gmail.com>
2026-02-02 23:57:32 +0530

    learnt order agnostic binary search

---
+++ Basic Java Recap/src/com/L4_Arrays_And_ArraysList/Binary_Search/Order_Agnostic_BS.java
@@ -0,0 +1,22 @@
+package com.L4_Arrays_And_ArraysList.Binary_Search;
+
+public class Order_Agnostic_BS {
+    /*
+    Order Agnostic Binary Search means when we don't know that array is sorted in Ascending or Descending way
+    if it is sorted in ascending way do it regular method
+    else just change the relational operators to it's opposite
+    target > array[mid] -> move left in descending
+    target < array[mid] -> move right in descending
+     */
+    public static void main(String[] args) {
+        int[] arr = {-45, -38, -21, -18, -9, -7, -3, 0, 7, 12, 23, 28, 31, 37, 39, 45, 54, 69, 89, 97, 100, 105};
+        int target = 97;
+
+
+    }
+
+    static int binarySearchAgnostic(int[] arr, int target) {
+
+    }
+
+}
