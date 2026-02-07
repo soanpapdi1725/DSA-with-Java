866b26714d75763a041f75fa87f3f875a664903d
Sonu Yadav <sy239370@gmail.com>
2026-02-07 22:40:40 +0530

    Find in Mountain Array made it more accurate comments added

--- Basic Java Recap/src/com/L4_Arrays_And_ArraysList/Binary_Search/Questions/FindInMountainArray1095.java
+++ Basic Java Recap/src/com/L4_Arrays_And_ArraysList/Binary_Search/Questions/FindInMountainArray1095.java
@@ -1,5 +1,27 @@
 package com.L4_Arrays_And_ArraysList.Binary_Search.Questions;
+//https://leetcode.com/problems/find-in-mountain-array/
 
+/*
+Ques:-
+Example 1:
+
+Input: mountainArr = [1,2,3,4,5,3,1], target = 3
+Output: 2
+Explanation: 3 exists in the array, at index=2 and index=5. Return the minimum index, which is 2.
+Example 2:
+
+Input: mountainArr = [0,1,2,4,2,1], target = 3
+Output: -1
+Explanation: 3 does not exist in the array, so we return -1.
+
+
+Approach:-
+1. find Peak Element
+2. Use BinarySearch on it for Ascending order by Providing the Array, target, start = 0, end = peak, isAsc = true
+   if it comes -1
+3. then run BinarySearch on it for Descending Order by Providing the Array, target, start = peak +1, end = array.length -1 , isAsc = false
+ if it still comes -1 then directly return it
+ */
 public class FindInMountainArray1095 {
     public static void main(String[] args) {
         int[] arr = {0, 5, 3, 1};
