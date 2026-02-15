0f2a75b1617ac760318eddb6b997cf58da01d330
Sonu Yadav <sy239370@gmail.com>
2026-02-15 22:24:43 +0530

    Bubble sort code completed

--- Java DSA/src/com/L4_Arrays_And_ArraysList/Sorting_Techniques/Bubble_Sort/BubbleSort.java
+++ Java DSA/src/com/L4_Arrays_And_ArraysList/Sorting_Techniques/Bubble_Sort/BubbleSort.java
@@ -1,7 +1,24 @@
 package com.L4_Arrays_And_ArraysList.Sorting_Techniques.Bubble_Sort;
 
+import java.util.Arrays;
+
 public class BubbleSort {
     public static void main(String[] args) {
-        
+        int[] arr = {5, 4, 3, 2, 1};
+        bubble(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    static void bubble(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n; i++) {
+            for (int j = 1; j < n - i; j++) {
+                if (arr[j] < arr[j - 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j - 1];
+                    arr[j - 1] = temp;
+                }
+            }
+        }
     }
 }
---
+++ Java DSA/src/com/L4_Arrays_And_ArraysList/Sorting_Techniques/Bubble_Sort/ValidAnagram.java
@@ -0,0 +1,19 @@
+package com.L4_Arrays_And_ArraysList.Sorting_Techniques.Bubble_Sort;
+
+public class ValidAnagram {
+    public static void main(String[] args) {
+        String s = "racecar", t = "carrace";
+        System.out.println(isAnagram(s, t));
+    }
+
+
+    static boolean isAnagram(String s, String t) {
+        boolean isAvailable = false;
+        for (int i = 0; i < s.length(); i++) {
+            for (int j = 0; j < t.length(); j++) {
+
+            }
+        }
+        return isAvailable;
+    }
+}
