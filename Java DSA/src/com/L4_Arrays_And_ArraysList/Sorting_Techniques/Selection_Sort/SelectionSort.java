751045cf4447cd1220f8428e43244ac32c52d092
Sonu Yadav <sy239370@gmail.com>
2026-02-17 23:18:15 +0530

    Selection sort is started and dry run completed

--- Java DSA/src/com/L4_Arrays_And_ArraysList/Sorting_Techniques/Selection_Sort/SelectionSort.java
+++ Java DSA/src/com/L4_Arrays_And_ArraysList/Sorting_Techniques/Selection_Sort/SelectionSort.java
@@ -3,6 +3,27 @@ package com.L4_Arrays_And_ArraysList.Sorting_Techniques.Selection_Sort;
 /*
 Selection Sort-> It is an sorting algorithm, in this it selects the element and put it in it's correct position
 
+Two ways
+1. Put Greatest element at it's correct position
+2. Put Smallest Element at it's correct position
+
+for example:-
+Put Greatest element at it's correct position
+Arr = [4,5,1,2,3]
+
+1. swap 5 to 3
+Arr = [4,3,1,2,5]
+
+2. swap 4 to 2
+Arr = [2,3,1,4,5]
+
+3. swap 3 to 1
+Arr = [2,1,3,4,5]
+
+4. swap 2 to 1
+Arr = [1,2,3,4,5]
+
+and done
  */
 public class SelectionSort {
 }
