86cbb4216fe3328a7e76ab9f7efc439002ab496f
Sonu Yadav <sy239370@gmail.com>
2026-02-17 23:07:48 +0530

    Selection sort is started

---
+++ Java DSA/src/com/L4_Arrays_And_ArraysList/Sorting_Techniques/Selection_Sort/SelectionSort.java
@@ -0,0 +1,8 @@
+package com.L4_Arrays_And_ArraysList.Sorting_Techniques.Selection_Sort;
+
+/*
+Selection Sort-> It is an sorting algorithm, in this it selects the element and put it in it's correct position
+
+ */
+public class SelectionSort {
+}
