098fc93e783209304d608c3efda8634293bb0ab1
Sonu Yadav <sy239370@gmail.com>
2026-02-27 00:07:15 +0530

    Find all duplicates in an array leetcode 442 done

--- Java DSA/src/com/L4_Arrays_And_ArraysList/Sorting_Techniques/Cyclic_Sort/Questions/Find_All_duplicates_442.java
+++ Java DSA/src/com/L4_Arrays_And_ArraysList/Sorting_Techniques/Cyclic_Sort/Questions/Find_All_duplicates_442.java
@@ -1,14 +1,18 @@
 package com.L4_Arrays_And_ArraysList.Sorting_Techniques.Cyclic_Sort.Questions;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
+//https://leetcode.com/problems/find-all-duplicates-in-an-array/
 public class Find_All_duplicates_442 {
     public static void main(String[] args) {
-
+        int[] nums = {4, 3, 2, 7, 8, 2, 3, 1};
+        System.out.println(Arrays.toString(findDuplicates(nums).toArray()));
     }
 
     static List<Integer> findDuplicates(int[] nums) {
-        List<Integer> duplicates = new ArrayList<>();
+        List<Integer> result = new ArrayList<>();
         int i = 0;
         while (i < nums.length) {
             int correctPos = nums[i] - 1;
@@ -20,10 +24,10 @@ public class Find_All_duplicates_442 {
         }
         for (int index = 0; index < nums.length; index++) {
             if ((index + 1) != nums[index]) {
-                duplicates.add(nums[index]);
+                result.add(nums[index]);
             }
         }
-        return duplicates;
+        return result;
     }
 
     static void swapNums(int[] nums, int first, int second) {
