37b20fab4ac9425deb34818dc0db6b4ac1639d95
Sonu Yadav <sy239370@gmail.com>
2026-03-03 02:29:32 +0530

    strings chapter done

--- Java DSA/src/com/L5_Strings_And_StringBuilder/Methods.java
+++ Java DSA/src/com/L5_Strings_And_StringBuilder/Methods.java
@@ -1,7 +1,27 @@
 package com.L5_Strings_And_StringBuilder;
 
+import java.util.Arrays;
+
 public class Methods {
     public static void main(String[] args) {
-        
+        /// String Methods
+        String name = "Sonu Yadav  is Good guy";
+        System.out.println(name.toLowerCase()); //-> not modifies the object of name creates a new object
+        System.out.println(name.toUpperCase()); // -> not modifies the object of name creates a new object
+        System.out.println("       Sonu    ".trim());
+        "Sonu".chars().forEach(System.out::println);
+        System.out.println(Arrays.toString("Sonu Yadav".toCharArray()));
+        System.out.println(name.codePointAt(0));
+        System.out.println(Arrays.toString(name.replace(" ", "-").split("-")));
+        System.out.println(name.substring(0, 10));
+        System.out.println(name.indexOf("u", 0, name.length() - 1));
+        System.out.println(name.contains("z"));
+
+
+        StringBuilder sB = new StringBuilder();
+        sB.append("Sonu");
+        sB.insert(sB.length(), " Yadav");
+        System.out.println(sB);
+        System.out.println(sB.capacity());
     }
 }
