20ced695eeee59a1c6f5736746e28b5b6b807247
Sonu Yadav <sy239370@gmail.com>
2026-03-06 18:11:50 +0530

    Recursion Main started
    # Conflicts:
    #	Java DSA/src/com/L7_Recursion/Basic_Recursion.java

--- Java DSA/src/com/L7_Recursion/NumbersExample.java
+++ Java DSA/src/com/L7_Recursion/NumbersExample.java
@@ -1,4 +1,41 @@
 package com.L7_Recursion;
 
+/*
+    Functions have same body & same work
+    what if we have to do same for 1,00,000 numbers
+ */
 public class NumbersExample {
+    public static void main(String[] args) {
+        /*
+            Print Number from 1 To 5
+            use function and callbacks
+
+         */
+        print1(1);
+    }
+
+    static void print1(int n) {
+        System.out.println(n);
+        print2(2);
+    }
+
+    static void print2(int n) {
+        System.out.println(n);
+        print3(3);
+    }
+
+    static void print3(int n) {
+        System.out.println(n);
+        print4(4);
+    }
+
+    static void print4(int n) {
+        System.out.println(n);
+        print5(5);
+    }
+
+    static void print5(int n) {
+        System.out.println(n);
+    }
+
 }
