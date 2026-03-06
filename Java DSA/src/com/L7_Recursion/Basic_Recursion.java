3493ce04a02267c0754bd6f9eca0fb08ed05950f
Sonu Yadav <sy239370@gmail.com>
2026-03-06 18:49:18 +0530

    Recursion use cases done

--- Java DSA/src/com/L7_Recursion/Basic_Recursion.java
+++ Java DSA/src/com/L7_Recursion/Basic_Recursion.java
@@ -1,6 +1,55 @@
 package com.L7_Recursion;
-/*
-Recursion is a function which calls itself again and again in a loop in order to solve the problem
-*/
-public class Recursion {
+
+public class Basic_Recursion {
+    public static void main(String[] args) {
+        message();
+        /*
+            Hello world
+            Hello world
+            Hello world
+            Hello world
+            Hello world
+         */
+    }
+
+    static void message() {
+        System.out.println("Hello world");
+        message1();
+        /*
+            Hello world
+            Hello world
+            Hello world
+            Hello world
+         */
+    }
+
+    static void message1() {
+        System.out.println("Hello world");
+        message2();
+        /*
+            Hello world
+            Hello world
+            Hello world
+         */
+    }
+
+    static void message2() {
+        System.out.println("Hello world");
+        message3();
+        /*
+            Hello world
+            Hello world
+         */
+    }
+
+    static void message3() {
+        System.out.println("Hello world");
+        message4();
+        // "Hello World"
+    }
+
+    static void message4() {
+        System.out.println("Hello world");
+    }
+
 }
--- Java DSA/src/com/L7_Recursion/Recursion.java
+++ Java DSA/src/com/L7_Recursion/Recursion.java
@@ -1,4 +1,32 @@
 package com.L7_Recursion;
 
+/*
+    Recursion is a function which calls itself again and again in a loop in order to solve the problem
+
+    Why to use Recursion->
+
+    ⭐ It helps us in solving bigger/Complex problems in a simpler way.
+
+    ⭐ You can convert recusrion Solution into Loops(Iteration) and vice versa
+
+    ⭐ Space complexity is not constant because of recursive calls
+
+    Space complexity -> Loops O(1)
+    Space Complexity -> recursion O(N)
+
+    ⭐ It helps in breaking down bigger problem into smaller problem
+*/
 public class Recursion {
+    public static void main(String[] args) {
+        print(1);
+    }
+
+    static void print(int n) {
+        if (n == 5) {
+            System.out.println(n);
+            return;
+        }
+        System.out.println(n);
+        print(n + 1);
+    }
 }
